86b5e8c24aa69278bb7a130ed3499723985d8621
kongkiat96 <kongkiat.0174@gmail.com>
2024-03-25 23:13:48 +0700

    Update config.core.php

--- core/config.core.php
+++ core/config.core.php
@@ -3,10 +3,10 @@
 define('DB_HOST', 'localhost');
 
 // MySQL database username
-define('DB_USERNAME', 'root');
+define('DB_USERNAME', 'systemit_new_demo_service_it');
 
 // MySQL database password
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'Uue29p5?0');
 
 // MySQL database name
-define('DB_NAME', 'new_service_it');
+define('DB_NAME', 'systemit_new_demo_service_it');
