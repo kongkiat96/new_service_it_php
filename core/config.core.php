7766891803493987e7dc0264e7c4762ae4063af3
kongkiat.b <kongkiat.b@srisawadpower.com>
2024-03-25 19:10:16 +0700

    Config Database

--- core/config.core.php
+++ core/config.core.php
@@ -3,10 +3,10 @@
 define('DB_HOST', 'localhost');
 
 // MySQL database username
-define('DB_USERNAME', 'systemit_skp_service');
+define('DB_USERNAME', 'systemit_new_demo_service_it');
 
 // MySQL database password
-define('DB_PASSWORD', 'Qld823?a5');
+define('DB_PASSWORD', 'Uue29p5?0');
 
 // MySQL database name
-define('DB_NAME', 'skp_service');
+define('DB_NAME', 'systemit_new_demo_service_it');
